76f05380bcb9ba5d086b0e3e32b23e151e726fa9
IliusSagar <sagar01830@gmail.com>
2023-12-14 23:44:13 +0600

    store method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        Student::create([
+            'name' => $request->name,
+            'course' => $request->course,
+            'email' => $request->email,
+            'phone' => $request->phone,
+        ]);
+
+    } // End Method
+
+
 }
